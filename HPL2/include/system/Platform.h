933f96d11dde3d55d740f8b0f11dc3338283da95
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-03-04 17:00:43 -0500

    Remove comment in Platform.h

--- HPL2/include/system/Platform.h
+++ HPL2/include/system/Platform.h
@@ -14,7 +14,6 @@ namespace hpl {
 
     //-----------------------------------------
 
-    // : public std::binary_function<cVideoMode, cVideoMode, bool>
     struct VideoComp {
         bool operator() (const cVideoMode& aVM1, const cVideoMode& aVM2) const;
     };
