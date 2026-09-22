3113d241fa33d30d8bb6a512b3aad033d95fc52e
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-22 03:04:22 -0400

    Allow loading of outlines for bitmap fonts

--- HPL2/sources/impl/SDLFontData.cpp
+++ HPL2/sources/impl/SDLFontData.cpp
@@ -57,7 +57,7 @@ bool cSDLFontData::CreateFromBitmapFile(const tWString &asFileName)
     TiXmlDocument *pXmlDoc = hplNew( TiXmlDocument,() );
     if(pXmlDoc->LoadFile(pFile)==false)
     {
-        Error("Couldn't load angle code font file '%s'\n",asFileName.c_str());
+        Error("Couldn't load angle code font file '%s', file does not exist or is not in XML format\n",cString::To8Char(asFileName).c_str());
         fclose(pFile);
         hplDelete(pXmlDoc);
         return false;
@@ -72,9 +72,16 @@ bool cSDLFontData::CreateFromBitmapFile(const tWString &asFileName)
     int lLineHeight = cString::ToInt(pCommonElem->Attribute("lineHeight"),0);
     int lBase = cString::ToInt(pCommonElem->Attribute("base"),0);
 
-    mfHeight = (float)lLineHeight;
+    //////////////////////////////////////////
+    // Load info
+    TiXmlElement *pInfoElem = pRootElem->FirstChildElement("info");
+    int lOutline = cString::ToInt(pInfoElem->Attribute("outline"),0) * 2;
 
-    mvSizeRatio.x = (float)lBase / (float)lLineHeight;//I think this is a not correct. Not sure what is done here :S
+    /////////////
+    // Apply outline to the scale
+    mfHeight = (float)(lLineHeight + lOutline);
+
+    mvSizeRatio.x = (float)(lBase + lOutline) / (float)(lLineHeight + lOutline);//I think this is a not correct. Not sure what is done here :S
     mvSizeRatio.y = 1;
 
     int lLargestGlyphId=-1;
@@ -115,6 +122,7 @@ bool cSDLFontData::CreateFromBitmapFile(const tWString &asFileName)
         if(pBitmap==NULL)
         {
             Error("Couldn't load bitmap %s for FNT file '%s'\n",cString::To8Char(sFilePath).c_str(),cString::To8Char(asFileName).c_str());
+            fclose(pFile);
             hplDelete(pXmlDoc);
             return false;
         }
@@ -184,7 +192,7 @@ bool cSDLFontData::CreateFromBitmapFile(const tWString &asFileName)
 
         //Create glyph and place it correctly.
         cGlyph *pGlyph = CreateGlyph(pImage,cVector2l(lXOffset,lYOffset),cVector2l(lW,lH),
-                                     cVector2l(lBase,lLineHeight),lAdvance);
+                                     cVector2l(lBase + lOutline,lLineHeight + lOutline),(lOutline + lAdvance * 2) / 2);
 
         mvGlyphs[lId] = pGlyph;
 
