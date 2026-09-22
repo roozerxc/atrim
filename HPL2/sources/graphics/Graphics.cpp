9e699996ad36afec150c7a5b1377f73469db710c
RoozerXC <45127566+roozerxc@users.noreply.github.com>
2026-09-22 03:16:12 -0400

    Add new Color Grading post effect shader

--- HPL2/sources/graphics/Graphics.cpp
+++ HPL2/sources/graphics/Graphics.cpp
@@ -29,6 +29,7 @@
 
 #include "graphics/PostEffect_Bloom.h"
 #include "graphics/PostEffect_ColorConvTex.h"
+#include "graphics/PostEffect_ColorGrading.h"
 #include "graphics/PostEffect_ImageTrail.h"
 #include "graphics/PostEffect_RadialBlur.h"
 
@@ -192,6 +193,7 @@ bool cGraphics::Init(    int alWidth, int alHeight, int alBpp, bool abFullscreen
         AddPostEffectType(hplNew( cPostEffectType_ColorConvTex, (this, apResources)) );
         AddPostEffectType(hplNew( cPostEffectType_ImageTrail, (this, apResources)) );
         AddPostEffectType(hplNew( cPostEffectType_RadialBlur, (this, apResources)) );
+        AddPostEffectType(hplNew( cPostEffectType_ColorGrading, (this, apResources)) );
     }
 
     Log("--------------------------------------------------------\n\n");
